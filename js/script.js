fc5ece8d89fef8dd301865ad008fbe537cd7a62e
sibagatovmihail <63659319+sibagatovmihail@users.noreply.github.com>
2023-01-24 20:15:37 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -5,3 +5,31 @@ menuBtn.addEventListener('click', () => {
     menuBtn.classList.toggle('open-menu')
     menu.classList.toggle('menu-open')
 })
+
+
+function onEntry(entry){
+    setTimeout(() => {
+        entry.forEach(change => {
+            if (change.isIntersecting) {
+                change.target.classList.add('_active')
+            }
+            else{
+                if(!change.target.classList.contains('anim-no-hide')){
+                    change.target.classList.remove('_active')
+                }
+                
+            }
+        })
+    }, 100);
+    
+}
+
+let options = {
+    threshold: [0]
+}
+let observser = new IntersectionObserver(onEntry, options);
+let elements = document.querySelectorAll('.animation-title, .card-animation');
+
+for (let elm of elements){
+    observser.observe(elm);
+}
